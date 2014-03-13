7f58ee922ebb588b67896d2bbc2ecfc4c11854de
VilhelmF <vfridriks@simnet.is>
2014-03-13 20:54:33 +0000

    Gerði ekkert merkilegt, bjó til smá test forrit og fixaði minor bugs í klösunum

--- testing.py
+++ testing.py
@@ -4,14 +4,32 @@ from Deck import *
 from Hand import *
 from Player import *
 
-
-print "Hello"
 Charmander = Card("Charmander", 39, 39, 90, ["Growl", "FireFang", "Flamethrower", "Inferno"], "fire", "water", "grass")
-print Charmander.name
-print Charmander.healthMax
-print Charmander.stamina
-print Charmander.attacks
-print Charmander.poketype
-print Charmander.weakness
-print Charmander.resistance
+Pikachu = Card("Pikachu", 35, 35, 100, ["Tackle", "QuickAttack", "ElectroBall", "Thunder"], "electric", "ground", "electric")
 
+
+
+
+
+
+def main():
+	print "In the blue corner, weighing 153 pounds, the one, the only Chaaarmander"
+	print Charmander.name
+	print Charmander.health
+	print Charmander.stamina
+	print Charmander.attacks
+	print Charmander.poketype
+	print Charmander.weakness
+	print Charmander.resistance
+	print "And his challenger, our furry electric ball. You might know him as vagina,,"
+	print "PPPIIIKKAACCHHUU"
+	print Pikachu.name
+	print Pikachu.health
+	print Pikachu.stamina
+	print Pikachu.attacks
+	print Pikachu.poketype
+	print Pikachu.weakness
+	print Pikachu.resistance
+
+if __name__ == "__main__":
+    main()
