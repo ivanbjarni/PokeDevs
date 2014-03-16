b7be631388abd750112c2e194258d792f8107701
ivanbjarni <ibj9@hi.is>
2014-03-16 14:40:29 +0000

    change testing, need pull

--- testing2.py
+++ testing2.py
@@ -12,7 +12,7 @@ presets = Presets()
 p1 = Player("p1")
 p1.mainCard = presets.gc("Charmander")
 p2 = Player("p2")
-p2.mainCard = presets.gc("Eevee")
+p2.mainCard = presets.gc("Blastoise")
 
 game = Main([p1,p2])
 game.gameLoop()
