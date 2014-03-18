af077ef4c8d4ded00ff1133d33b0fbf48785e34d
VilhelmF <vfridriks@simnet.is>
2014-03-18 12:53:48 +0000

    Pokemons&Attacks

--- Presets.py
+++ Presets.py
@@ -354,7 +354,7 @@ class Presets(object):
 		112	: Card("Rhydon", 105, 100, [self.ga("Megahorn"), self.ga("Earthquake"), self.ga("ScaryFace"), self.ga("TakeDown")], "ground", "grass", "normal"),
 		113	: Card("Chansey", 250, 120, [self.ga("DoubleSlap"), self.ga("LightScreen"), self.ga("HealingWish"), self.ga("TakeDown")], "normal", "ground", "none"),
 		114	: Card("Tangela", 65, 120, [self.ga("MegaDrain"), self.ga("StunSpore"), self.ga("VineWhip"), self.ga("Slam")], "grass", "fire", "water"),
-		115	: Card("Kangashkan", 105, 100, [self.ga("FakeOut"), self.ga("Rage"), self.ga("ChipAway"), self.ga("DizzyPunch")], "normal", "ground", "psychic"),
+		115	: Card("Kangaskhan", 105, 100, [self.ga("FakeOut"), self.ga("Rage"), self.ga("ChipAway"), self.ga("DizzyPunch")], "normal", "ground", "psychic"),
 		116	: Card("Horsea", 30, 75, [self.ga("WaterGun"), self.ga("Bubble"), self.ga("BubbleBeam"), self.ga("Smokescreen")], "water", "grass", "water"),
 		117	: Card("Seadra", 55, 90, [self.ga("WaterGun"), self.ga("HydroPump"), self.ga("Brine"), self.ga("Smokescreen")], "water", "grass", "water"),
 		118	: Card("Goldeen", 45, 80, [self.ga("Peck"), self.ga("WaterPulse"), self.ga("FuryAttack"), self.ga("Waterfall")], "water", "electric", "fire"),
