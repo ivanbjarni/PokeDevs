68262993f53892379f1efd635af58f848816f2ef
Ívan Bjarni Jónsson <ibj9@hi.is>
2014-03-24 18:42:23 +0000

    added functionality for pokemon counters

---
+++ initTextFiles.py
@@ -0,0 +1,12 @@
+from Presets import *
+
+p = Presets()
+
+def createPokemonCounterFile(name):
+	with open(name, "a") as myfile:
+		for i in xrange(1,152):
+			myfile.write(p.gc(i).name+" 0\n")
+		myfile.close()
+
+createPokemonCounterFile("pokemonKillers.txt")
+createPokemonCounterFile("pokemonVictims.txt")
