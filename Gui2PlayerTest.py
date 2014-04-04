539dd8150f647ad6cc9bf62eaf573af6e011c835
SaevarIngi <saevarin@gmail.com>
2014-04-04 17:21:47 +0000

    getting fixes

--- Gui2PlayerTest.py
+++ Gui2PlayerTest.py
@@ -1714,8 +1714,10 @@ class MainFrame(wx.Frame):
 			self.game.drawInv(self.game.players[1])
 		if self.game.players[1].mainCard.isDead():
 			# Choose a new pokemon to put out if the currently chosen one is dead
+			print self.game.players[1].mainCard
 			newCard = self.game.chooseCardAI(self.game.players[1], self.game.players[0])
 			self.game.players[1].mainCard = newCard
+			print self.game.players[1].mainCard
 			self.game.textLog.append('Opponent put out ' + newCard.name + '\n')
 			self.updateStatus()
 			self.gamePanel.switchCPUpokemon(newCard)
